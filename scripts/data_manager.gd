1d6006b1b0c7c94d1e23e9753b908461c223e679
emanisgrand <tenhitcombo@gmail.com>
2024-08-14 01:29:56 -0400

    integrated calendar functionality from main app ui

--- scripts/data_manager.gd
+++ scripts/data_manager.gd
@@ -8,5 +8,32 @@ var exercise_list = {
 	"LEG": ["BBB Squats", "Bulgarian Splits", "Deadlift", "M. Deadlift"]
 }
 
+var recorded_workout_days = {}
+
 func get_exercises_for_muscle_group(muscle_group: String) -> Array:
 	return exercise_list.get(muscle_group, [])
+
+func record_workout_day(date):
+	var date_key = "%d-%02d-%02d" % [date.year, date.month, date.day]
+	recorded_workout_days[date_key] = true
+	save_data()
+
+func is_workout_day_recorded(date):
+	var date_key = "%d-%02d-%02d" % [date.year, date.month, date.day]
+	return date_key in recorded_workout_days
+
+func save_data():
+	var save_file = FileAccess.open("user://workout_data.save", FileAccess.WRITE)
+	save_file.store_var(recorded_workout_days)
+	save_file.close()
+
+func load_data():
+	if FileAccess.file_exists("user://workout_data.save"):
+		var save_file = FileAccess.open("user://workout_data.save", FileAccess.READ)
+		recorded_workout_days = save_file.get_var()
+		save_file.close()
+	else:
+		recorded_workout_days = {}
+
+func _ready():
+	load_data()
